0985e37aaca0d2a1b421560de10b62b85431b604
Vinoth <vinoth186@gmail.com>
2019-07-25 11:06:15 +0530

    Create Git_Stale_Branches.sh

---
+++ Git_Stale_Branches.sh
@@ -0,0 +1,10 @@
+#!bash
+
+echo Merged branches 
+for branch in `git branch -r --merged | grep -v HEAD`; do
+echo -e `git log --no-merges -n 1 --format="%ci, %cr, %an, %ae, "  $branch | head -n 1` \\t$branch; 
+done | sort -r 
+
+# To delete 
+
+git push origin --delete SSMClientICE_CR_D_RHEL7_WAS9
