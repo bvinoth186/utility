fa44208d6f1ce479e3ff7aa40969bc860ec4b764
Vinoth <vinoth186@gmail.com>
2019-07-25 11:14:10 +0530

    Update Git_Stale_Branches.sh

--- Git_Stale_Branches.sh
+++ Git_Stale_Branches.sh
@@ -1,5 +1,8 @@
 #!bash
 
+# To store the git credentials in cache 
+git config --global credential.helper 'cache --timeout 7200'
+
 echo Merged branches 
 for branch in `git branch -r --merged | grep -v HEAD`; do
 echo -e `git log --no-merges -n 1 --format="%ci, %cr, %an, %ae, "  $branch | head -n 1` \\t$branch; 
